d780cdb6009aa9ce4ad339f5ead5e56f8af9992a
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-08-01 14:33:36 +0300

    Create NavBar.js

---
+++ src/components/NavBar.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import "./NavBar.css";
+import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from "react-icons/fa"
+import HerryImg from "../assets/Herry-img.jpg"
+
+const NavBar = () => {
+  const [click, setClick] =  useState(false);
+  const [color, setColor] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const changeColor = () => {
+    if(window.scrollY >= 100) {
+        setColor(true);
+    }else {
+        setColor(false);
+    }
+  }
+
+  window.addEventListener("scroll", changeColor);
+
+  return (
+    <div className={color ? "header header-bg" : "header"}>
+        <Link to={"/"}>
+            <h1>Portfolio</h1>
+            {/* <img className="herry-img" src={HerryImg} alt="HerryImg"/> */}
+        </Link>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li>
+                <Link to={"/"}>Home</Link>
+            </li>
+            <li>
+                <Link to={"/Projects"}>Projects</Link>
+            </li>
+            <li>
+                <Link to={"/About"}>About</Link>
+            </li>
+            <li>
+                <Link to={"/Contact"}>Contact</Link>
+            </li>
+        </ul>
+        <div className="hamburger" onClick={handleClick}>
+            {click ? (
+            <FaTimes size={20} style={{color:"#fff"}}/>
+            ) : (
+            <FaBars size={20} style={{color:"#fff"}}/>
+            )}
+        </div>
+    </div>
+  )
+}
+
+export default NavBar;
