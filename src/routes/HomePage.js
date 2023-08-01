3ee594aa9dce98eaa02cf07b6fa55af3433b0811
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-08-01 14:33:54 +0300

    Create HomePage.js

---
+++ src/routes/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../components/NavBar';
+import HomeImg from '../components/HomeImg'
+const HomePage = () => {
+  return (
+    <div>
+        <NavBar />
+        <HomeImg />
+    </div>
+  )
+}
+
+export default HomePage;
