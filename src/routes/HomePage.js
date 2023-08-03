bece6e4c123175b912dba07a94e2b96d39cb8c10
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-08-03 21:38:36 +0300

    Update HomePage.js

--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import NavBar from '../components/NavBar';
 import HomeImg from '../components/HomeImg'
+import Footer from '../components/Footer';
+
 const HomePage = () => {
   return (
     <div>
         <NavBar />
         <HomeImg />
+        <Footer />
     </div>
   )
 }
