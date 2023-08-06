da167abba35bfba153bcc121e5f4085e060606ce
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-08-06 23:00:49 +0300

    Update AboutPage.js

--- src/routes/AboutPage.js
+++ src/routes/AboutPage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import PagesImg from '../components/PagesImg';
+import AboutContent from '../components/AboutContent';
 import Footer from '../components/Footer';
 
 const AboutPage = () => {
   return (
     <div>
       <NavBar />
+      <PagesImg heading="ABOUT." text="Meet Hermon Gebrehiwot: Crafting Inspiring Full Stack Development Experiences."/>
+      <AboutContent />
       <Footer />
     </div>
   )
