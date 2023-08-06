44e551d1a15aa8cd428417f020357a9e6ff797d5
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-08-06 23:00:59 +0300

    Update ProjectsPage.js

--- src/routes/ProjectsPage.js
+++ src/routes/ProjectsPage.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import PagesImg from '../components/PagesImg';
+import Work from '../components/Work';
+import PricingCards from '../components/PricingCards';
 import Footer from '../components/Footer';
-
 
 const ProjectsPage = () => {
   return (
     <div>
       <NavBar />
+      <PagesImg heading="PROJECTS." text="Explore some of my Projects: Unleashing the Power of Innovation."/>
+      <Work />
+      <PricingCards />
       <Footer />
     </div>
   )
