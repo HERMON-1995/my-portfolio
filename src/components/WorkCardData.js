78392e3a933f6458f12e510b7aa313607a589bcc
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-08-06 23:00:45 +0300

    Create WorkCardData.js

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,29 @@
+import MathMagician from "../assets/Math-magician.mp4";
+import ToDoList from "../assets/To-do-list.mp4";
+import AwesomeBooks from "../assets/Awesome-books.mp4";
+
+const ProjectCardData = [
+    {
+        imgsrc: MathMagician,
+        title: "Math Magician",
+        text: "Single Page App that allows users to make simple calculations and read a random math-related quote. Built with: React, Redux, JavaScript, Html/CSS",
+        live: "https://hermon-math-magician-app.onrender.com/",
+        source: "https://github.com/HERMON-1995/Math-magician-project"
+    },
+    {
+        imgsrc: ToDoList,
+        title: "To Do List",
+        text: "Web application that allows users to create, manage and remove tasks or items that they need to complete. Built with: HTML/CSS, JavaScript, ES6",
+        live: "https://hermon-1995.github.io/To-do-list-project/dist/",
+        source: "https://github.com/HERMON-1995/To-do-list-project"
+    },
+    {
+        imgsrc: AwesomeBooks,
+        title: "Awesome Books",
+        text: "Web application that displays a list of books and allows users to add and remove books from the list board.  Built with: Html-5/CSS3, JavaScript, ES6",
+        live: "https://hermon-1995.github.io/Awesome-books-ES6-project/",
+        source: "https://github.com/HERMON-1995/Awesome-books-ES6-project"
+    }
+]
+
+export default ProjectCardData;
