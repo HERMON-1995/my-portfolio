67e6334ae3f152926f0c1c8b6ae912f4c452a5db
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-08-06 23:00:54 +0300

    Update ContactPage.js

--- src/routes/ContactPage.js
+++ src/routes/ContactPage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import PagesImg from '../components/PagesImg';
+import Form from '../components/Form';
 import Footer from '../components/Footer';
 
 const ContactPage = () => {
   return (
     <div>
       <NavBar />
+      <PagesImg heading="CONTACT." text="Whether you're looking to collaborate on an exciting project or just want to connect over shared interests, I'm always excited to make new connections. Reach out to me, and let's explore the possibilities of crafting innovative solutions that make a real impact."/>
+      <Form />
       <Footer />
     </div>
   )
