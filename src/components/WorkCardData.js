dca7e2d2010f4fd02f1f079e313a975f61bd2677
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-08-07 14:39:42 +0300

    Update WorkCardData.js

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,8 +1,25 @@
+import LeaderBoard from "../assets/Leader-board.mp4";
+import WebDevelopment from "../assets/Web-development.mp4";
+import MyMovies from "../assets/My-movies.mp4";
 import MathMagician from "../assets/Math-magician.mp4";
 import ToDoList from "../assets/To-do-list.mp4";
 import AwesomeBooks from "../assets/Awesome-books.mp4";
 
 const ProjectCardData = [
+    {
+        imgsrc: WebDevelopment,
+        title: "Web Development",
+        text: "Online courses website for web development from scratch where users can choose and take free online web-development courses. Built with Html, CSS and JavaScript.",
+        live: "https://hermon-1995.github.io/Web-development-capstone-project/",
+        source: "https://github.com/HERMON-1995/Web-development-capstone-project"
+    },
+    {
+        imgsrc: MyMovies,
+        title: "My Movies",
+        text: "Web app that uses an external TV maze API to create a personalized movie application involving building two user interfaces and utilizing Webpack and Jest dependencies. Built with: HTML/CSS and JavaScript",
+        live: "https://hermon-1995.github.io/My-movies-capstone-project/dist/",
+        source: "https://github.com/HERMON-1995/My-movies-capstone-project"
+    },
     {
         imgsrc: MathMagician,
         title: "Math Magician",
@@ -23,6 +40,13 @@ const ProjectCardData = [
         text: "Web application that displays a list of books and allows users to add and remove books from the list board.  Built with: Html-5/CSS3, JavaScript, ES6",
         live: "https://hermon-1995.github.io/Awesome-books-ES6-project/",
         source: "https://github.com/HERMON-1995/Awesome-books-ES6-project"
+    },
+    {
+        imgsrc: LeaderBoard,
+        title: "Leader Board",
+        text: "Web application that displays player names and scores retrieved from an API. It uses a table format and allows sorting by score and searching for specific players. Built with: HTML, CSS, and JavaScript",
+        live: "https://hermon-1995.github.io/Leaderboard-list-app-project/dist/",
+        source: "https://github.com/HERMON-1995/Leaderboard-list-app-project"
     }
 ]
 
