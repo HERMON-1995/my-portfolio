7ebc5543d9563e64b12c8471908292febc9ea01f
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-08-07 23:32:35 +0300

    Create Skills.js

---
+++ src/components/Skills.js
@@ -0,0 +1,96 @@
+import "./Skills.css";
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <div className="skills">
+        <h1 className="skills-heading">Hard Skills</h1>
+        <div className="hard-skills-div">
+            <div className="skills-card">
+             <img src="https://static.frontendmasters.com/assets/courses/2023-01-23-react-performance/thumb.webp" alt="React" className="skillsimg"/>
+             <p>React.js</p>
+            </div>
+
+            <div className="skills-card">
+             <img src="https://networksynapse.net/wp-content/uploads/2020/06/redux-1232x615.png" alt="Redux" className="skillsimg"/>
+             <p>Redux</p>
+            </div>
+
+            <div className="skills-card">
+             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw7zM8roLB0obzD_3xJhexF4alyE-6tQqIQLRyKtGwvZisrLNZxozbZYwT9o8CncxjDNM&usqp=CAU" alt="JavaScript" className="skillsimg"/>
+             <p>JavaScript</p>
+            </div>
+
+            <div className="skills-card">
+             <img src="https://i0.wp.com/tolustar.com/wp-content/uploads/2020/02/html-css.jpg?ssl=1" alt="Html/css" className="skillsimg"/>
+             <p>Html5/CSS3</p>
+            </div>
+
+            <div className="skills-card">
+             <img src="https://www.osiztechnologies.com/asset/images/inner-pages/ruby/ruby-on-rails-development-company.png" alt="Ruby on Rails" className="skillsimg"/>
+             <p>Ruby on Rails</p>
+            </div>
+
+            <div className="skills-card">
+             <img src="https://cottonbureau.com/mockup?vid=5673274&detail=1" alt="Tailwind.css" className="skillsimg"/>
+             <p>Tailwind CSS</p>
+            </div>
+
+            <div className="skills-card">
+             <img src="https://www.persistent.com/wp-content/uploads/2016/02/blog-banner-Is-your-Node.js-application-production-ready-scaled.jpg" alt="Node.js" className="skillsimg"/>
+             <p>Node.js</p>
+            </div>
+
+            <div className="skills-card">
+             <img src="https://blog.logrocket.com/wp-content/uploads/2023/01/import-svgs-next-js-app.png" alt="Next.js" className="skillsimg"/>
+             <p>Next.js</p>
+            </div>
+        </div>
+
+        <h1 className="skills-heading">Soft Skills</h1>
+        <div className="soft-skills-div">
+            <div className="skills-card">
+             <img src="https://w7.pngwing.com/pngs/324/178/png-transparent-problem-solving-problem-based-learning-educational-technology-ecommerce-platform-collaboration-experience-area-thumbnail.png" alt="Leardership&teamwork" className="skillsimg"/>
+             <p>Leardership & Teamwork</p>
+            </div>
+
+            <div className="skills-card">
+             <img src="https://png.pngtree.com/png-vector/20220127/ourmid/pngtree-big-isolated-employee-working-in-office-interior-workplace-flat-vector-illustration-png-image_4367511.png" alt="Problem solving" className="skillsimg"/>
+             <p>Problem Solving</p>
+            </div>
+
+            <div className="skills-card">
+             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_iFnlEoXug5Ysy5W_8iFIuueIT1ghe3ZgUlz2hkmPX2e7UDUyK2iL354raPvPCW2D7DI&usqp=CAU" alt="Communication" className="skillsimg"/>
+             <p>Communication</p>
+            </div>
+
+            <div className="skills-card">
+             <img src="https://cdn-icons-png.flaticon.com/512/1897/1897866.png" alt="Time Managment" className="skillsimg"/>
+             <p>Time Management</p>
+            </div>
+
+            <div className="skills-card">
+             <img src="https://media.licdn.com/dms/image/D4E12AQF4qT7Vqcf9hA/article-cover_image-shrink_600_2000/0/1686150381333?e=2147483647&v=beta&t=NEXAj3_9VtGDdAX9XxXIdG6POpFui7sUhgKlpO1gJ88" alt="Continuous Learning" className="skillsimg"/>
+             <p>Continuous Learning</p>
+            </div>
+
+            <div className="skills-card">
+             <img src="https://thumbs.dreamstime.com/b/web-264288755.jpg" alt="Attention to Detail" className="skillsimg"/>
+             <p>Attention to Detail</p>
+            </div>
+
+            <div className="skills-card">
+             <img src="https://cdn-icons-png.flaticon.com/512/2973/2973138.png" alt="Creativity" className="skillsimg"/>
+             <p>Creativity</p>
+            </div>
+
+            <div className="skills-card">
+             <img src="https://image.pngaaa.com/157/3912157-middle.png" alt="Strategic Planning" className="skillsimg"/>
+             <p>Strategic Planning</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Skills;
