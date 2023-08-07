ae53e1160b594bbceae1dd34cb167b5499f15452
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-08-07 14:39:26 +0300

    Update AboutContent.js

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -9,11 +9,11 @@ const AboutContent = () => {
     <div className="about-div">
         <div className="left">
             <h1>WHO AM I?</h1>
-            <p>My fascination with technology started at a young age when I got my hands on my first computer. The joy of exploring this digital playground ignited a curiosity that never ceased to grow. As I delved deeper into the realms of software and programming, I realized that the power to create and solve problems lay right at my fingertips.</p>
+            <p>From a Customer Success Manager in the electronics industry, as I delved deeper into the world of electronics sales, I noticed a growing trend in the industry—online platforms were becoming the go-to marketplace for buyers and sellers alike. With this realization, I saw a chance to bridge the gap between sales and full-stack development to create a seamless and unique experience for both parties.</p>
 
             <p>The early days of my career were filled with challenges, but I welcomed them with open arms. I joined a dynamic startup, where I had the opportunity to work on diverse projects ranging from e-commerce platforms to social networking apps. These experiences not only expanded my technical proficiency but also taught me the value of teamwork, time management, and innovation.</p>
 
-            <p>Beyond the confines of code, I have a penchant for adventure and exploration. When I'm not glued to my computer screen, you can find me hiking through scenic trails or trying my hand at photography. These pursuits rejuvenate my mind and keep the creative juices flowing.</p>
+            <p>Over the past several months, I've embarked on a remarkable journey, collaborating remotely with individuals spread across three different time zones—UTC-6, UTC+1, and UTC+5. This unique experience has not only honed my adaptability but also made me stand out from the crowd. Working seamlessly across these diverse time zones showcases my exceptional ability to connect and excel in a globalized world.</p>
 
             <p>Thank you for visiting my portfolio website. Let's make technology an instrument of boundless creativity and progress. Together, we can create magic!</p>
             <Link to="/Contact">
